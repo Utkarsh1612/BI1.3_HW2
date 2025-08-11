cbeb9bb2d12da6563bc6e95782b39b6662b69c1f
Utkarsh Tyagi <utkarshtyagi1612@gmail.com>
2025-08-11 13:11:08 +0530

    add hosted backend API

--- src/components/HotelNames.jsx
+++ src/components/HotelNames.jsx
@@ -3,11 +3,16 @@ import useFetch from "../useFetch";
 
 const HotelNames = () => {
   const [message, setMessage] = useState("");
-  const { data, loading, error } = useFetch("http://localhost:3000/hotels");
+  const { data, loading, error } = useFetch(
+    "https://be-4-4-hw-2-hotels-xi.vercel.app/hotels"
+  );
   const handleDelete = async (id) => {
-    const response = await fetch(`http://localhost:3000/hotels/${id}`, {
-      method: "DELETE",
-    });
+    const response = await fetch(
+      `https://be-4-4-hw-2-hotels-xi.vercel.app/hotels${id}`,
+      {
+        method: "DELETE",
+      }
+    );
     const data = response.json();
     if (data) {
       setMessage("Hotel deleted successfully! ✅");
