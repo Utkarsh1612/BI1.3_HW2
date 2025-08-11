20577bd797ba5aa3f382ef32c33433496514fdfc
Utkarsh Tyagi <utkarshtyagi1612@gmail.com>
2025-08-11 12:45:51 +0530

    BI1.3_HW2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HotelNames from "./components/HotelNames";
+
+const App = () => {
+  return (
+    <div>
+      <HotelNames />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/HotelNames.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useFetch from "../useFetch";
+
+const HotelNames = () => {
+  const [message, setMessage] = useState("");
+  const { data, loading, error } = useFetch("http://localhost:3000/hotels");
+  const handleDelete = async (id) => {
+    const response = await fetch(`http://localhost:3000/hotels/${id}`, {
+      method: "DELETE",
+    });
+    const data = response.json();
+    if (data) {
+      setMessage("Hotel deleted successfully! ✅");
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+    }
+  };
+  return (
+    <>
+      <h1>Hotels</h1>
+      <ul>
+        {data &&
+          data.map((hotel) => (
+            <li key={hotel._id}>
+              {hotel.name}{" "}
+              <button onClick={() => handleDelete(hotel._id)}>Delete</button>
+            </li>
+          ))}
+      </ul>
+      {message && <p>{message}</p>}
+    </>
+  );
+};
+
+export default HotelNames;
